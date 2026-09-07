23649982b2bcb8b9519692b87e1d85231d57f82c
Derek McTavish <derek@mctavishdynamics.com>
2026-09-08 04:17:52 +0700

    Update documentation and test specifications

--- src/parser.c
+++ src/parser.c
@@ -6285,7 +6285,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_piton(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 0,
+      .minor_version = 1,
       .patch_version = 0,
     },
   };
